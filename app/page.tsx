09912819b43b2159ba98f053f985417df8012bda
blw3232 <162641700+blw3232@users.noreply.github.com>
2026-03-05 21:52:45 -0500

    add X link

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,19 @@ export default function Home() {
   priority
   style={{ borderRadius: "50%" }}
 />
-<div className="links">
-  <a href="https://www.linkedin.com/in/brendanweitz">LinkedIn</a> |{" "}
-  <a href="https://x.com/bweitz1">X</a> |{" "}
+<p className="links">
+  (
+  <a href="https://www.linkedin.com/in/brendanlweitz" target="_blank" rel="noreferrer">
+    LinkedIn
+  </a>
+  ,{" "}
+  <a href="https://x.com/YOURHANDLE" target="_blank" rel="noreferrer">
+    X
+  </a>
+  ,{" "}
   <a href="mailto:bweitz1@gmail.com">Email</a>
+  )
+</p>
 </div>
         </p>
 
